ed0bc3236cd3c7e468d4ab21b15cf40a34e323c1
LiChuanmin <1915540982@qq.com>
2022-06-01 16:39:44 +0800

    0601

---
+++ MySwift/Detail/Find/Controller/LeeTestViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LeeTestViewController.swift
+//  MySwift
+//
+//  Created by LiChuanmin on 2022/5/30.
+//  Copyright © 2022 LCM. All rights reserved.
+//
+
+import UIKit
+
+class LeeTestViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+ 
+
+}
